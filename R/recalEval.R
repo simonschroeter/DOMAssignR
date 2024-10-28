04deaff55f5fe27a3a7740a86f23655204d17eec
simonschroeter <78800019+simonschroeter@users.noreply.github.com>
2024-10-28 21:39:53 +0100

    Update recalEval.R
    Integrated attempts at fixing recal errors.

--- R/recalEval.R
+++ R/recalEval.R
@@ -10,7 +10,7 @@ recalEval <- function(import,
                        recal_result){
   `%>%` <- magrittr::`%>%`
   #WERE ANY UNSUCCESSFUL?
-  print(ifelse(any(sapply(recal_result, function(x){x[[3]]}) != "Successfully recalibrated."),
+  print(ifelse(any(!sapply(recal_result, function(x){x[[3]]}) %in% c("Successfully recalibrated.","Successfully recalibrated with only one homologous series. Proceed with caution.")),
          "Some samples contain recalibration errors, please check and remove before continuing.",
          "All samples okay to conitnue."))
   #CHECK IF ALL SAMPLES COULD BE RECALIBRATED
@@ -18,14 +18,16 @@ recalEval <- function(import,
   #  print(paste0("Sample",i, ": ", recal_result[[i]][3]))
   #}
   #
-  if(any(sapply(recal_result, function(x){x[[3]]}) != "Successfully recalibrated.")){
+  if(any(!sapply(recal_result, function(x){x[[3]]}) %in% c("Successfully recalibrated.","Successfully recalibrated with only one homologous series. Proceed with caution."))){
   for (i in 1:length(recal_result)){
     if(i==1){print("The following samples could not be recalibrated:")}
-    if(recal_result[[i]][[3]] != "Successfully recalibrated."){
+    if(recal_result[[i]][[3]] %in% c("Successfully recalibrated.","Successfully recalibrated with only one homologous series. Proceed with caution.")){
       print(paste0("Sample",i, ": ", recal_result[[i]][4]))
     }}
-  if(!any(sapply(recal_result, function(x){x[[3]]}) != "Successfully recalibrated.")) {print("none")}
+  if(!any(!sapply(recal_result, function(x){x[[3]]}) %in% c("Successfully recalibrated.","Successfully recalibrated with only one homologous series. Proceed with caution."))) {print("none")}
   }
+  print("_____________________________")
+  print("Spectra info:")
   for (i in 1:length(import)){
     print(paste("Sample",i, import[[i]][[3]],"contains", import[[i]][[1]]$Mono %>% nrow(), "monoisotopic masses above noiselevel =", round(import[[i]][[2]])))
     }
